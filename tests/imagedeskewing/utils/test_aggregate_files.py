b5e749728fb5a0e1fc300a9522a679dbc4aeb779
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-05-25 01:11:23 -0400

    adding new unit tests for counting all file types

--- tests/imagedeskewing/utils/test_aggregate_files.py
+++ tests/imagedeskewing/utils/test_aggregate_files.py
@@ -1,6 +1,8 @@
 import os
 import unittest
-from imagedeskewing.utils.aggregate_files import count_file_type
+from collections import defaultdict
+
+from imagedeskewing.utils.aggregate_files import count_file_type, count_all_file_types
 
 
 class TestAggregateFiles(unittest.TestCase):
@@ -8,12 +10,12 @@ class TestAggregateFiles(unittest.TestCase):
         """
         Set up the testing environment before each test case.
 
-        Create a test directory and set the file extension to use for testing.
+        Create a test directory and set the file extensions to use for testing.
         """
         self.test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_files")
         if not os.path.exists(self.test_dir):
             os.makedirs(self.test_dir)
-        self.test_file_extension = ".txt"
+        self.test_file_extensions = [".txt", ".jpg", ".png"]
 
     def tearDown(self):
         """
@@ -26,17 +28,19 @@ class TestAggregateFiles(unittest.TestCase):
         if os.path.exists(self.test_dir):
             os.rmdir(self.test_dir)
 
-    def create_dummy_files(self, num_files):
+    def create_dummy_files(self, num_files, file_extension):
         """
-        Create the specified number of dummy files in the test directory.
+        Create the specified number of dummy files with a specific extension in the test directory.
 
         Parameters
         ----------
         num_files : int
             The number of dummy files to create.
+        file_extension : str
+            The file extension for the dummy files. E.g., '.tiff'.
         """
         for i in range(num_files):
-            with open(os.path.join(self.test_dir, f"dummy{i}{self.test_file_extension}"), 'w') as f:
+            with open(os.path.join(self.test_dir, f"dummy{i}{file_extension}"), 'w') as f:
                 f.write("This is a dummy file.")
 
     def test_count_file_type(self):
@@ -46,9 +50,9 @@ class TestAggregateFiles(unittest.TestCase):
         The function should correctly count the number of '.txt' files in the test directory.
         """
         num_files = 5
-        self.create_dummy_files(num_files)
+        self.create_dummy_files(num_files, self.test_file_extensions[0])
 
-        txt_file_count = count_file_type(self.test_dir, self.test_file_extension)
+        txt_file_count = count_file_type(self.test_dir, self.test_file_extensions[0])
         self.assertEqual(txt_file_count, num_files)
 
     def test_count_file_type_no_files(self):
@@ -58,8 +62,24 @@ class TestAggregateFiles(unittest.TestCase):
         The function should return 0 when counting the number of '.jpg' files in the test directory,
         since no '.jpg' files were created.
         """
-        jpg_file_count = count_file_type(self.test_dir, ".jpg")
+        jpg_file_count = count_file_type(self.test_dir, ".asdf")
         self.assertEqual(jpg_file_count, 0)
+
+    def test_count_all_file_types(self):
+        """
+        Test that the function counts the correct number of files of all types.
+
+        The function should correctly count the number of '.txt', '.jpg', and '.png' files in the test directory.
+        """
+        expected_counts = defaultdict(int)
+
+        for extension in self.test_file_extensions:
+            num_files = 5
+            self.create_dummy_files(num_files, extension)
+            expected_counts[extension] = num_files
+
+        actual_counts = count_all_file_types(self.test_dir)
+        self.assertEqual(actual_counts, expected_counts)
 
 
 if __name__ == "__main__":
