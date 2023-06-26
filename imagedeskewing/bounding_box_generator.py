5b0f333f5d8054d0cdffedfb0595cd6df5933d00
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-06-26 02:26:22 -0500

    fixed bounding box generation

--- imagedeskewing/bounding_box_generator.py
+++ imagedeskewing/bounding_box_generator.py
@@ -1,14 +1,13 @@
-import torch
 import numpy as np
+import torch
+import supervision as sv
 
 from groundingdino.util.inference import Model
-
-from utils.image import Image
 
 
 class BoundingBoxGenerator:
     """
-    A class used to generate bounding boxes using GroundingDINO.
+    A class used to generate bounding boxes for objects in images.
 
     Attributes
     ----------
@@ -19,10 +18,8 @@ class BoundingBoxGenerator:
 
     Methods
     -------
-    load_model(config_path: str, weight_path: str) -> None
-        Load the GroundingDINO model.
-    annotate_image(image_path: str, text_prompt: str, box_threshold: float, text_threshold: float) -> np.ndarray
-        Load an image, generate bounding boxes, and annotate the detections.
+    find_objects(image_path: str, text_prompt: str, box_threshold: float, text_threshold: float) -> sv.Detections
+        Locate objects in an image and return the bounding boxes.
     """
 
     def __init__(self, config_path: str, weight_path: str):
@@ -47,36 +44,33 @@ class BoundingBoxGenerator:
         """Load the GroundingDINO model."""
         return Model(model_config_path=config_path, model_checkpoint_path=weight_path, device=device)
 
-    def annotate_image(self, image_path: str, text_prompt: str, box_threshold: float,
-                       text_threshold: float) -> np.ndarray:
+    def find_objects(self, image: np.ndarray, text_prompt: str, box_threshold: float,
+                     text_threshold: float) -> sv.Detections:
         """
-        Load an image, generate bounding boxes, and annotate the detections.
+        Locate objects in an image and return the bounding boxes.
 
         Parameters
         ----------
-        image_path : str
-            The path to the image file.
+        image : np.ndarray
+            The image data in RGB format.
         text_prompt : str
-            The text prompt for the prediction.
+            The text prompt for the prediction. This can be a series of phrases separated by a comma or a single phrase.
         box_threshold : float
-            The box threshold for the prediction.
+            The box threshold for the prediction. This is the confidence threshold for the bounding boxes.
         text_threshold : float
-            The text threshold for the prediction.
+            The text threshold for the prediction. This is the similarity confidence threshold for the
+            words to use as the labels.
 
         Returns
         -------
-        np.ndarray
-            Annotated image with bounding boxes and segmentation annotations.
+        sv.Detections
+            Detections object containing the bounding boxes in xyxy format and confidence scores.
         """
-        image = Image(image_path)
         detections, phrases = self.model.predict_with_caption(
-            image=image.as_array(),
+            image=image,
             caption=text_prompt,
             box_threshold=box_threshold,
             text_threshold=text_threshold
         )
 
-        print(detections)
-
-        return None # TODO: Implement this
-
+        return detections
