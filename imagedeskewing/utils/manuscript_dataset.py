530946a75e0c0d73298946f220845bfdc507844a
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-08-13 18:30:27 -0400

    creating dataset class for images

---
+++ imagedeskewing/utils/manuscript_dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import DataLoader, Dataset
+
+
+class ManuscriptDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
