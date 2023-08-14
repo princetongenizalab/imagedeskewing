7d01ef5de6ed2f9bf404ab445e704ad87b06b6ff
EthanHaque <43977335+EthanHaque@users.noreply.github.com>
2023-08-14 01:45:10 -0400

    testing out using imagenet

--- imagedeskewing/utils/manuscript_dataset.py
+++ imagedeskewing/utils/manuscript_dataset.py
@@ -1,12 +1,25 @@
 from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+import os
+from PIL import Image
+import torch
 
 
 class ManuscriptDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self, csv_file, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.transform = transform
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     def __getitem__(self, idx):
-        pass
+        img_name = self.data.iloc[idx, 1]
+        image = Image.open(img_name)
+        skew_angle = self.data.iloc[idx, 2]
+        sample = {'image': image, 'skew_angle': skew_angle}
+
+        if self.transform:
+            sample['image'] = self.transform(sample['image'])
+
+        return sample
